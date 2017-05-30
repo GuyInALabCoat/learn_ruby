74396a9a153287aec88d7ad7c450c58ca58ee8a2
Dmytro <dmytropolitov@gmail.com>
2017-05-30 04:28:18 -0400

    All tests passed

--- 04_pig_latin/pig_latin_spec.rb
+++ 04_pig_latin/pig_latin_spec.rb
@@ -69,4 +69,13 @@ describe "#translate" do
   # * write a test asserting that capitalized words are still capitalized (but with a different initial capital letter, of course)
   # * retain the punctuation from the original phrase
 
+  it "retain capitalization" do
+    s = translate("The Quick Brown Fox")
+    expect(s).to eq("Ethay Ickquay Ownbray Oxfay")
+  end
+
+  it "retain punctuation" do
+    s = translate("The: Quick. Brown? Fox!")
+    expect(s).to eq("Ethay: Ickquay. Ownbray? Oxfay!")
+  end
 end
